aa5de763f8441ce3e72caa56d0f6a92b69aa9955
iqtidar <iqtikhan@yahoo.com>
2021-08-17 13:36:37 +0500

    First Commit

---
+++ resources/views/layout/js.blade.php
@@ -0,0 +1,22 @@
+<script src="../../../app-assets/vendors/js/vendors.min.js"></script>
+<script src="../../../app-assets/js/core/app-menu.js"></script>
+<script src="../../../app-assets/js/core/app.js"></script>
+<script>
+    $(window).on('load', function() {
+        if (feather) {
+            feather.replace({
+                width: 14,
+                height: 14
+            });
+        }
+    });
+    $('#confirm-delete').on('show.bs.modal', function(e) {
+        $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+    });
+    $(document).ready(function() {
+        $('select[data-option-id]').each(function() {
+            $(this).val($(this).data('option-id'));
+        });
+    });
+
+</script>
