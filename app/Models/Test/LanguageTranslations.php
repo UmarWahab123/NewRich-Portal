5281de1cb25a3e063b8ed760572b8f234b40867b
Qaiser Abbas <qaiserabbas102@gmail.com>
2021-08-25 12:40:08 +0500

    delete old version

--- app/Models/Test/LanguageTranslations.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models\Test;
-use Illuminate\Database\Eloquent\Model;
-class LanguageTranslations extends Model
-{
-    protected $guarded = array();
-    protected $table = 'language_translations';
-}
